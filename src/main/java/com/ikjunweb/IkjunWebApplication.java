79427479f6f6e48434c23f0d6efaf96aa15a20ae
130moodTRBL <sonic6679@naver.com>
2022-12-20 16:40:23 +0900

    first

---
+++ src/main/java/com/ikjunweb/IkjunWebApplication.java
@@ -0,0 +1,13 @@
+package com.ikjunweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IkjunWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IkjunWebApplication.class, args);
+	}
+
+}
